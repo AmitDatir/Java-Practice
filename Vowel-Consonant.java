825ec477668794c3d0c9bdae3f62b573797852be
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-06-24 23:43:32 +0530

    Update Vowel-Consonant.java

--- Vowel-Consonant.java
+++ Vowel-Consonant.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class vowelConsonant {
+public class vowelConsonant1 {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter any character: ");
@@ -17,3 +17,33 @@ public class vowelConsonant {
 //Output:
 Enter any character: d
 d is consonant.
+
+    
+    
+
+    
+    
+import java.util.Scanner;
+public class vowelConsonant2 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any character: ");
+        String ch = input.next();
+        switch(ch){
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+                System.out.println(ch + " is vowel.");
+                break;
+            default:
+                System.out.println(ch +" is consonant.");
+        }
+    }
+}
+
+
+//Output:
+Enter any character: i
+i is vowel.
