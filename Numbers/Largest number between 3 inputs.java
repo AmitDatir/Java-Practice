8c9f12702ba959b069dafb627b11d63ddf2d017c
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-06-27 20:33:08 +0530

    Update Largest number between 3 inputs.java

--- Numbers/Largest number between 3 inputs.java
+++ Numbers/Largest number between 3 inputs.java
@@ -1,6 +1,45 @@
 //largest number between 3 inputs:
 import java.util.Scanner;
-public class largestNumber {
+public class largestNumber1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 1st number to check: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter 2nd number to check: ");
+        int number2 = input.nextInt();
+        System.out.print("Enter 3rd number to check: ");
+        int number3 = input.nextInt();
+        int largest;
+        
+        if(number1 >= number2 && number1>=number3){
+            largest = number1;
+        }else if(number2 >= number1 && number2>=number3){
+            largest = number2;
+        }else{
+            largest = number3;
+        }
+        System.out.println("Largest number from given input is: " + largest );
+}
+}
+
+
+
+/*
+Output:
+Enter 1st number to check: 523
+Enter 2nd number to check: -45
+Enter 3rd number to check: 584521
+Largest number from given input is: 584521
+*/
+
+
+
+
+
+
+
+import java.util.Scanner;
+public class largestNumber2 {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter 1st number to check: ");
@@ -29,7 +68,8 @@ public class largestNumber {
 }
 
 
-/*Output:
+/*
+Output:
 Enter 1st number to check: 2512
 Enter 2nd number to check: 20
 Enter 3rd number to check: 152
