60b7be101bce822ef7fcbbfa50a48ec8a95dc47f
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-02 23:28:42 +0530

    Create  Check for leap year.java

---
+++ General/Check for leap year.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String args[]) {
+        int year;
+        boolean leap = false;
+        System.out.print("Enter a year to check: ");
+        Scanner input = new Scanner(System.in);
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    leap = true;
+                } else {
+                    leap = false;
+                }
+            } else {
+                leap = true;
+            }
+        } else {
+            leap = false;
+        }
+
+        if (leap) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
+
+/*
+Output:
+1.
+Enter a year to check: 1111
+1111 is not a leap year.
+2.
+Enter a year to check: 13
+13 is not a leap year.
+3.
+Enter a year to check: 1900
+1900 is not a leap year.
+4.
+Enter a year to check: 2020
+2020 is a leap year.
+*/
