4d6176fd9f7c826065b51237d8acf87e80699ca3
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-04 17:37:53 +0530

    Update Character is alphabet or not.java

--- String-Char/Character is alphabet or not.java
+++ String-Char/Character is alphabet or not.java
@@ -55,3 +55,44 @@ Enter any character to check: 6
 Enter any character to check: abc
 Enter only single character.
 */
+
+
+
+
+//Using isAlphabetic() method:
+import java.util.Scanner;
+public class AlphabetNoAlpha {
+    private char ch;
+
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any character to check: ");
+        String s1 = input.next();
+        AlphabetNoAlpha v1 = new AlphabetNoAlpha();
+        v1.setCharacter(s1);
+        v1.isAlpha(v1.getCharacter());
+    }
+
+    public void setCharacter(String s) {
+        if (s.length() == 1) {
+            ch = s.charAt(0);
+        } else {
+            System.out.println("Enter only single character.");
+        }
+    }
+
+
+    public char getCharacter() {
+        return ch;
+    }
+
+
+    public void isAlpha(char ch) {
+        if(Character.isAlphabetic(ch) ){
+            System.out.println(ch + " is an Alphabet.");
+        } else{
+            System.out.println(ch + " is not an Alphabet.");
+        }
+    }
+}
