c99af7f3e01f6461737bb6217909d40e416349b2
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-07 23:14:42 +0530

    Create Sum of natural numbers.java

---
+++ Numbers/Sum of natural numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SumNatural {
+
+    public static void main(String args[]) {
+        int num,sum=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number upto which we need to find sum of natural numbers: ");
+        num = input.nextInt();
+        
+        for(int i=1;i<=num;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum of numbers is: " + sum);
+    }
+}
+
+
+
+/*
+Output:
+Enter a number upto which we need to find sum of natural numbers: 100
+Sum of numbers is: 5050
+*/
