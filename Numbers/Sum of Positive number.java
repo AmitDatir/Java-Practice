8fb1f8057a8284fbec0330c8a597454839943eb9
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-09 23:50:02 +0530

    Create Sum of Positive number.java

---
+++ Numbers/Sum of Positive number.java
@@ -0,0 +1,86 @@
+//using while loop:
+import java.util.Scanner;
+public class SumPositive {
+
+    public static void main(String args[]) {
+        int sum = 0;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to add:");
+        int number = input.nextInt();
+        
+        while(number >= 0){
+            sum+=number;
+            System.out.print("Enter a number to add:");
+            number = input.nextInt();
+        }
+        
+        if(number<0){
+            System.out.println("You entered a negative number, so exiting the program.");
+        }
+            
+        System.out.println("Sum of all positive numbers entered is = " + sum);
+    }
+}
+
+
+/*
+Output:
+Enter a number to add:1
+Enter a number to add:2
+Enter a number to add:3
+Enter a number to add:4
+Enter a number to add:5
+Enter a number to add:6
+Enter a number to add:7
+Enter a number to add:8
+Enter a number to add:9
+Enter a number to add:10
+Enter a number to add:-11
+You entered a negative number, so exiting the program.
+Sum of all positive numbers entered is = 55
+*/
+
+
+
+//using do-while loop:
+import java.util.Scanner;
+public class SumPositive {
+
+    public static void main(String args[]) {
+        int number = 0, sum = 0;
+        Scanner input = new Scanner(System.in);
+        
+        do{
+            sum+=number;
+            System.out.print("Enter a number to add:");
+            number = input.nextInt();
+        } while(number>=0);
+        
+        if(number<0){
+            System.out.println("You entered a negative number, so exiting the program.");
+        }
+            
+        System.out.println("Sum of all positive numbers entered is = " + sum);
+        input.close();
+    }
+}
+
+
+/*
+Output:
+Enter a number to add:10
+Enter a number to add:9
+Enter a number to add:8
+Enter a number to add:7
+Enter a number to add:6
+Enter a number to add:5
+Enter a number to add:4
+Enter a number to add:3
+Enter a number to add:2
+Enter a number to add:1
+Enter a number to add:0
+Enter a number to add:-1
+You entered a negative number, so exiting the program.
+Sum of all positive numbers entered is = 55
+*/
