9dcd3eb269934a974cb5a1f5a55d73de8ec5dbfb
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-14 23:30:39 +0530

    Update and rename LCM of two numbers.ja to LCM of two numbers.java

--- Numbers/LCM of two numbers.java
+++ Numbers/LCM of two numbers.java
@@ -38,3 +38,35 @@ Enter 2 numbers to find LCM.
 LCM of numbers 81 & 153 is: 1377
 */
 
+
+//With using GCD:
+import java.util.Scanner;
+public class LCM2 {
+
+    public static void main(String args[]) {
+        int number1, number2,gcd = 1,lcm;
+        System.out.println("Enter 2 numbers to find LCM.");
+        Scanner input = new Scanner(System.in);
+        System.out.print("1st: ");
+        number1 = input.nextInt();
+        System.out.print("2nd: ");
+        number2 = input.nextInt();
+        
+        for (int i=1; i <= number1 && i <= number2; i++){
+            if(number1 % i == 0 && number2 % i == 0){
+                gcd = i;
+            }
+        }
+        
+        lcm = (number1 * number2) / gcd;
+        System.out.printf("GCD & LCM of two numbers %d & %d is: %d & %d", number1, number2, gcd, lcm);
+    }
+}
+
+/*
+Output:
+Enter 2 numbers to find LCM.
+1st: 81
+2nd: 153
+GCD & LCM of two numbers 81 & 153 is: 9 & 1377
+*/
