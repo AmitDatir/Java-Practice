b882a1096a2e331524f38b90879e08bf12ce52a3
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-16 23:36:06 +0530

    Create alphabets.java

---
+++ String-Char/alphabets.java
@@ -0,0 +1,35 @@
+//Capital:
+public class AlphabetsCaps {
+
+    public static void main(String args[]) {
+        char c;
+        for(c = 'A'; c<='Z'; ++c){
+            System.out.print(c + " ");
+        }
+    }
+}
+
+
+/*
+Output:
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+*/
+
+
+
+//Small:
+public class AlphabetsSmall {
+
+    public static void main(String args[]) {
+        char c;
+        for(c = 'a'; c<='z'; ++c){
+            System.out.print(c + " ");
+        }
+    }
+}
+
+
+/*
+Output:
+a b c d e f g h i j k l m n o p q r s t u v w x y z  
+*/
