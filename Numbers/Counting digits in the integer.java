b541a5fdb4c88fee52b55407d929483bd3f72066
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-17 23:16:11 +0530

    Update Counting digits in the integer.java

--- Numbers/Counting digits in the integer.java
+++ Numbers/Counting digits in the integer.java
@@ -1,1 +1,52 @@
+//using while loop:
+import java.util.Scanner;
+public class Count1 {
+    public static void main(String args[]) {
+        int number,count=0;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer to check: ");
+        number = input.nextInt();
+        
+        while(number != 0){
+            number /= 10;
+            count++;
+        }
+        
+        System.out.println("Count of digits in given integer is: " + count);
+    }
+}
 
+
+
+/*
+Output:
+Enter an integer to check: 12345678
+Count of digits in given integer is: 8
+*/
+
+
+
+//using for loop:
+import java.util.Scanner;
+public class Count1 {
+    public static void main(String args[]) {
+        int number,count=0;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer to check: ");
+        number = input.nextInt();
+        
+        for(; number!=0; number/=10, ++count);
+        
+        System.out.println("Count of digits in given integer is: " + count);
+    }
+}
+
+
+
+/*
+Output:
+Enter an integer to check: 000234561
+Count of digits in given integer is: 6
+*/
