cb8830b0c5c704d9d353dde406d544b0f6656aee
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-18 22:02:07 +0530

    Update reverse a number.java

--- Numbers/reverse a number.java
+++ Numbers/reverse a number.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class Count1 {
     public static void main(String args[]) {
-        int number, reveresed=0, digit;
+        int number, reversed=0, digit;
         
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the original number: ");
@@ -10,12 +10,12 @@ public class Count1 {
         
         while(number!=0){
             digit = number % 10;
-            reveresed = reveresed * 10 + digit;
+            reversed = reversed * 10 + digit;
             
             number/=10;
         }
         
-        System.out.println("Reveresed number is: " + reveresed);
+        System.out.println("Reversed number is: " + reversed);
     }
 }
 
@@ -23,5 +23,32 @@ public class Count1 {
 /*
 Output:
 Enter the original number: 654321
-Reveresed number is: 123456
+Reversed number is: 123456
+*/
+
+
+//Using for loop:
+import java.util.Scanner;
+public class Count1 {
+    public static void main(String args[]) {
+        int number, reversed=0, digit;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the original number: ");
+        number = input.nextInt();
+        
+        for(; number!=0; number/=10){
+            digit = number % 10;
+            reversed = reversed * 10 + digit;
+        }
+        
+        System.out.println("Reversed number is: " + reversed);
+    }
+}
+
+
+/*
+Output:
+Enter the original number: 456123
+Reversed number is: 321654
 */
