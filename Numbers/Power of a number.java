01c967f0661ac114bcc2acf1375d87dc04d25f3c
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-20 21:52:19 +0530

    Update Power of a number.java

--- Numbers/Power of a number.java
+++ Numbers/Power of a number.java
@@ -41,3 +41,61 @@ Enter the base number: 5
 Enter the exponent number: 3
 Power of base 5.0 & exponent 3.0 is: 125.0
 */
+
+
+
+//Using for loop:
+import java.util.Scanner;
+public class Power2 {
+    
+    private int result=1;
+    
+    public static void main(String args[]) {
+        
+        double base, exponent;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the base number: ");
+        base = input.nextDouble();
+        System.out.print("Enter the exponent number: ");
+        exponent = input.nextDouble();
+        
+        Power2 p2 = new Power2();
+        p2.setPower(base,exponent);
+    
+        System.out.println("Power of base " + base + " & exponent " + exponent + " is: " + p2.getPower());
+    }
+        
+    public void setPower(double a, double b){
+        for (; b!=0; b--){
+            result*=a;
+        }
+    }
+    
+    public double getPower(){
+        return result;
+    }
+    
+}
+
+
+
+//Using pow() function:
+import java.util.Scanner;
+public class Power2 {
+    
+    private int result=1;
+    
+    public static void main(String args[]) {
+        
+        double base, exponent;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the base number: ");
+        base = input.nextDouble();
+        System.out.print("Enter the exponent number: ");
+        exponent = input.nextDouble();
+        
+        System.out.printf("Power of base " + base + " & exponent " + exponent + " is: " + Math.pow(base,exponent));
+    }
+}
