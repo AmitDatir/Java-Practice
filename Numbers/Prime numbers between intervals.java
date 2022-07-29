fe85ece29faca1bdd1f859158213ee18573730f5
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-07-30 00:05:19 +0530

    Update and rename Prime number between intervals.java to Prime numbers between intervals.java

--- Numbers/Prime numbers between intervals.java
+++ Numbers/Prime numbers between intervals.java
@@ -5,9 +5,9 @@ public class PrimeNumberInterval {
         
         
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a lower limit: ");
+        System.out.print("Enter a lower limit: ");
         min = input.nextInt();
-        System.out.println("Enter a upper limit: ");
+        System.out.print("Enter a upper limit: ");
         max = input.nextInt();
         
         while (min<max){
@@ -19,10 +19,17 @@ public class PrimeNumberInterval {
                 }
             }
             if (!result && min != 0 && min != 1){
-            System.out.println(min + ", ");
-            
-            ++min;
+            System.out.print(min + " ");
             }
+            ++min;
         }
     }
 }
+
+
+/*
+Output:
+Enter a lower limit: 0
+Enter a upper limit: 50
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+*/
