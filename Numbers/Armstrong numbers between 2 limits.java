dfa8facafb1aeccf78ea2788f55734a8485e015d
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-08-04 10:59:06 +0530

    Update Armstrong numbers between 2 limits.java

--- Numbers/Armstrong numbers between 2 limits.java
+++ Numbers/Armstrong numbers between 2 limits.java
@@ -55,3 +55,58 @@ Enter a lower limit to find Armstrong numbers: 5
 Enter a upper limit: 1526
 Enter limits greater than 9.
 */
+
+
+
+
+import java.util.Scanner;
+
+public class ArmstrongNumber2 {
+    public static void main(String args[]) {
+        int min,max, num;
+        
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a lower limit to find Armstrong numbers: ");
+        min = input.nextInt();
+        System.out.print("Enter a upper limit: ");
+        max = input.nextInt();
+        
+        if (min/10 != 0 || max/10 != 0){
+            System.out.print("\nArmstrong numbers between given numbers are: ");
+            
+            for (num = min; num < max; num++){
+                
+                if (isArmstrong(num)){
+                    System.out.print(num + " ");
+                }
+            }
+        }
+        else {
+            System.out.println("Enter limits greater than 9.");
+        }
+    }
+    
+    static boolean isArmstrong(int arm){
+        int result = 0, n = 0, original = arm;
+                
+        while (original!=0){
+            original/=10;
+            n++;
+        }
+        original = arm;
+                
+        while (original!=0){
+            int remainder = original % 10;
+                result += Math.pow(remainder,n);
+                original/=10;
+            }
+                
+            if (result == arm){
+                return true;
+            } 
+            else {
+                return false;
+            }
+    }
+}
