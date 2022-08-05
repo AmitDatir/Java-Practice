6cb9516bf7bb5a7408bc476ffe84cbedaec18818
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-08-05 22:35:28 +0530

    Update and rename Factor of a number.java to Factors of a number.java

--- Numbers/Factors of a number.java
+++ Numbers/Factors of a number.java
@@ -56,3 +56,39 @@ output:
 Enter a number to find factors: -60
 Factors of given number are: -60 -30 -20 -15 -12 -10 -6 -5 -4 -3 -2 -1 1 2 3 4 5 6 10 20 30 12 15 60 
 */
+
+
+
+//Combined:
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String args[]) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to find factors: ");
+        int num = input.nextInt();
+        
+        System.out.print("Factors of given number are: ");
+        
+        if (num < 0){
+        for (int i=num; i<=Math.abs(num); i++){
+            if ( i == 0){
+                continue;
+            } else {
+                if (num % i == 0){
+                System.out.print( i + " ");
+                }
+              }
+        }
+        } else if (num > 0){
+            for (int i=1; i<=num; i++){
+            if (num % i == 0){
+                System.out.print( i + " ");
+            }
+        }
+        } else{
+            System.out.println("No factos!!!! You entered 0.");
+        }
+    }
+}
