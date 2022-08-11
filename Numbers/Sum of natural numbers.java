2710eb207b49aec1476f49c7c4435fd04a0bfb30
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-08-11 22:52:47 +0530

    Update Sum of natural numbers.java

--- Numbers/Sum of natural numbers.java
+++ Numbers/Sum of natural numbers.java
@@ -46,3 +46,34 @@ public class SumNatural {
 
 
 //Using Recursion:
+import java.util.Scanner;
+
+public class NaturalSum {
+    public static void main(String args[]) {
+        int num; 
+        long result;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a last natural number to find sum: ");
+        num = input.nextInt();
+        
+        result = sum(num);
+        System.out.println("Sum upto number " + num + " = " + result);
+    }
+    
+    static long sum(int num1){
+        if (num1 != 0){
+            long sum1 = num1 + sum(num1-1);
+            return sum1;
+        } else{
+            return num1;
+        }
+    }
+}
+
+
+/*
+Output:
+Enter a last natural number to find sum: 100
+Sum upto number 100 = 5050
+*/
