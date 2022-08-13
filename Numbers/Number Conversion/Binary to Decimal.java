794a21b5e42d8e8f2312af5b826e2ea37cdbc296
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-08-13 19:47:36 +0530

    Create Binary to Decimal.java

---
+++ Numbers/Number Conversion/Binary to Decimal.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BinarytoDecimal {
+    public static void main(String args[]) {
+        long binary;
+        int decimal; 
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter binary number to convert into decimal: ");
+        binary = input.nextLong();
+        
+        decimal = binaryToDecimal(binary);
+        
+        System.out.println(binary + " in binary = " + decimal + " in decimal");
+    }
+    
+    public static int binaryToDecimal(long num){
+        int decimal = 0, i=0;
+        long remainder;
+        
+        while(num!=0){
+            remainder = num % 10;
+            num /= 10;
+            decimal += remainder * Math.pow(2,i);
+            i++;
+     
+        }
+        return decimal;
+    }
+}
+
+
+
+/*
+Output:
+1.
+Enter binary number to convert into decimal: 110110111
+110110111 in binary = 439 in decimal
+2.
+Enter binary number to convert into decimal: 11100011
+1115600011 in binary = 1411 in decimal
+*/
