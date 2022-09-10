3d26c2469f2c4344b4c84bc6317eb0ba24e1fa9e
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-09-10 23:46:42 +0530

    Update Pascal'sTriangle.java

--- General/Pascal'sTriangle.java
+++ General/Pascal'sTriangle.java
@@ -1,1 +1,34 @@
+public class PascalsTriangle {
+    public static void main(String args[]) {
+        int rows=7, coeff=1;
+        
+        for(int i=0; i<rows; i++){
+            for(int j=1; j< rows-i; j++){
+                System.out.print(" ");
+            }
+            
+            for(int j=0; j<=i; j++){
+                if(j==0 || i==0){
+                    coeff=1;
+                } else{
+                    coeff = coeff * (i-j+1)/j;
+                }
+                System.out.printf("%4d",coeff);
+            }
+            
+            System.out.println();
+        }
+    }
+}
 
+
+/*
+Output:
+         1
+        1   1
+       1   2   1
+      1   3   3   1
+     1   4   6   4   1
+    1   5  10  10   5   1
+   1   6  15  20  15   6   1
+*/
