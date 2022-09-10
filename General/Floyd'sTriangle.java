5f9102cd57b78f892280333a7d6aed2548a8187f
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-09-11 00:00:15 +0530

    Update Floyd'sTriangle.java

--- General/Floyd'sTriangle.java
+++ General/Floyd'sTriangle.java
@@ -2,7 +2,7 @@ public class FloydsTriangle {
     public static void main(String args[]) {
         int rows=7, coeff=1;
         
-        for(int i=0; i<rows; i++){
+        for(int i=1; i<=rows; i++){
             for(int j=1; j<= i; j++){
                 System.out.print(coeff + " ");
                 coeff++;
@@ -22,5 +22,6 @@ Output:
 4 5 6 
 7 8 9 10 
 11 12 13 14 15 
-16 17 18 19 20 21
+16 17 18 19 20 21 
+22 23 24 25 26 27 28 
 */
