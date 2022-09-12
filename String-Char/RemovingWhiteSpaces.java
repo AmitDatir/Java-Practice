a8183b5b05e211442acc03e791bd87f1fc9f71ca
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-09-12 23:05:09 +0530

    Update RemovingWhiteSpaces.java

--- String-Char/RemovingWhiteSpaces.java
+++ String-Char/RemovingWhiteSpaces.java
@@ -1,1 +1,19 @@
+//using replaceAll method:
+import java.util.Scanner;
+public class WhiteSpace {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any string value: ");
+        String str = input.nextLine();
+        
+        str = str.replaceAll("\\s", "");
+        System.out.print("String after replacement: " + str);
+    }
+}
 
+
+/*
+Output:
+Enter any string value: j a v a i s a w e s o m e
+String after replacement: javaisawesome
+*/
