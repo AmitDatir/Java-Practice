dd0caf4929fd73b8210275b578d95e54b5e80be4
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-09-15 23:58:05 +0530

    Create StringtoDate.java

---
+++ String-Char/StringtoDate.java
@@ -0,0 +1,39 @@
+//predefined formatters:
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class TimeString1 {
+    public static void main(String args[]) {
+        String str = "2022-09-15";
+        LocalDate date = LocalDate.parse(str, DateTimeFormatter.ISO_DATE);
+        
+        System.out.println(date);
+    }
+}
+
+
+/*
+Output:
+2022-09-15
+*/
+
+
+
+//pattern formatters:
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+public class TimeString2 {
+    public static void main(String args[]) {
+        String str = "September 15, 2022";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH);
+        LocalDate date = LocalDate.parse(str, formatter);
+        
+        System.out.println(date);
+    }
+}
+
+
+/*
+Output:
+2022-09-15
+*/
