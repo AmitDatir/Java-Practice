adbabf2006c9c7c1d81dd36aeb82efdc31c0bd06
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-09-24 23:56:18 +0530

    Update GetCurrentDateTime.java

--- General/GetCurrentDateTime.java
+++ General/GetCurrentDateTime.java
@@ -1,1 +1,11 @@
+//Default format:
+import java.time.LocalDateTime;
+public class CurrentDateTime1 {
+    public static void main(String args[]) {
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("Current Date & Time is: " + current);
+    }
+}
 
+
+//Output: Current Date & Time is: 2022-09-24T18:25:03.129902674
