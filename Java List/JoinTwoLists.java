e1d2b0b79808eb7873b1d76bbb7ef1a7883ab4ed
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-11-03 14:31:15 +0530

    Update JoinTwoLists.java

--- Java List/JoinTwoLists.java
+++ Java List/JoinTwoLists.java
@@ -1,1 +1,92 @@
+//using addAll():
+import java.util.ArrayList;
+import java.util.List;
 
+public class JoinLists1 {
+    public static void main(String args[]) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("C");
+        list1.add("C++");
+        list1.add("C#");
+        
+        List<String> list2 = new ArrayList<>();
+        list2.add("Java");
+        list2.add("Python");
+        list2.add("PHP");
+        list2.add("Ruby");
+        
+        List<String> joined = new ArrayList<>();
+        joined.addAll(list1);
+        joined.addAll(list2);
+        
+        System.out.println("List-1: " + list1);
+        System.out.println("List-2: " + list2);
+        System.out.println("Joined List: " + joined);
+    }
+}
+
+
+/* 
+Output:
+List-1: [C, C++, C#]
+List-2: [Java, Python, PHP, Ruby]
+Joined List: [C, C++, C#, Java, Python, PHP, Ruby]
+*/
+
+
+
+//using union():
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.collections.ListUtils;
+
+public class JoinLists2 {
+    public static void main(String args[]) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("C");
+        list1.add("C++");
+        list1.add("C#");
+        
+        List<String> list2 = new ArrayList<>();
+        list2.add("Java");
+        list2.add("Python");
+        list2.add("PHP");
+        list2.add("Ruby");
+        
+        List<String> joined = ListUtils.union(list1, list2);
+        
+        System.out.println("List-1: " + list1);
+        System.out.println("List-2: " + list2);
+        System.out.println("Joined List: " + joined);
+    }
+}                                                           //Output??????
+
+
+
+
+//using stream:
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JoinLists3 {
+    public static void main(String args[]) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("C");
+        list1.add("C++");
+        list1.add("C#");
+        
+        List<String> list2 = new ArrayList<>();
+        list2.add("Java");
+        list2.add("Python");
+        list2.add("PHP");
+        list2.add("Ruby");
+        
+        List<String> joined =  Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+        
+        System.out.println("List-1: " + list1);
+        System.out.println("List-2: " + list2);
+        System.out.println("Joined List: " + joined);
+    }
+}
