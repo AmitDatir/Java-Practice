5e54c3070fe9f1e9f690c423dd0ffd2d29848306
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-11-07 23:13:47 +0530

    Update CurrentWorkingDirectory.java

--- General/CurrentWorkingDirectory.java
+++ General/CurrentWorkingDirectory.java
@@ -1,1 +1,23 @@
+//Current working directory:
+public class CurrentDirectory1 {
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir");
+        System.out.println("Working Directory = " + path);
 
+    }
+}
+
+//Output:  Working Directory = /home
+
+
+
+
+//Using Path:
+import java.nio.file.Paths;
+public class CurrDirectory2 {
+    public static void main(String[] args) {
+        String path = Paths.get("").toAbsolutePath().toString();
+        System.out.println("Working Directory = " + path);
+
+    }
+}
