4727731dd673defd9d310e698ba3a451291998ad
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-11-10 23:09:17 +0530

    Update ArrayToSet.java

--- Java Set/ArrayToSet.java
+++ Java Set/ArrayToSet.java
@@ -1,1 +1,27 @@
+//Array to Set:
+import java.util.*;
+public class ArraySet1 {
+    public static void main(String args[]) {
+        String arr[] = {"a", "b", "c", "1", "2", "3"};
+        Set<String> set = new HashSet<>(Arrays.asList(arr));
+        System.out.println("Set: " + set);
+    }
+}
 
+
+
+//Output: Set: [a, 1, b, 2, c, 3]
+
+
+
+
+//Using stream:
+import java.util.*;
+public class ArraySet2 {
+    public static void main(String args[]) {
+        String arr[] = {"a", "b", "c", "1", "2", "3"};
+        Set<String> set = new HashSet<>(Arrays.stream(arr).collect(Collectors.toSet()));
+        System.out.println("Set: " + set);
+    }
+}
+//error????
