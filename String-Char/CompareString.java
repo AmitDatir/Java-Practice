13c45ba775e3680e1468017c57c91a90bf574912
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-11-21 20:05:05 +0530

    Create CompareString.java

---
+++ String-Char/CompareString.java
@@ -0,0 +1,36 @@
+public class CompareString1 {
+    public static void main(String args[]) {
+        String str1 = "Amit";
+        String str2 = "Amt";
+        
+        if(str1 == str2){                                 //compares the object, not the value
+            System.out.println("Equal Strings");
+        } else{
+            System.out.println("Not Equal Strings");
+        }
+    }
+}
+
+
+//Output: Not Equal Strings
+
+
+//using equals():
+public class CompareString2 {
+    public static void main(String args[]) {
+        String str1 = new String("Amit");        //as we are using string constructor, we need to use equals method to compare strings
+        String str2 = new String("Amit");
+        
+        if(str1.equals(str2)){                        //compares the value
+            System.out.println("Equal Strings");
+        } else{
+            System.out.println("Not Equal Strings");
+        }
+    }
+}
+
+
+//Output:  Equal Strings
+
+
+
