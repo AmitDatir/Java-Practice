bdeef4820b1c0130587439d47c033769f59762c4
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-12-05 22:48:43 +0530

    Update 1704_Determine_If_String_Halves_Are_Alike.java

--- LeetCode/1704_Determine_If_String_Halves_Are_Alike.java
+++ LeetCode/1704_Determine_If_String_Halves_Are_Alike.java
@@ -1,5 +1,5 @@
 /*
-1704. Determine if String Halves Are Alike
+1704. Determine if String Halves Are Alike :- https://leetcode.com/problems/determine-if-string-halves-are-alike/
 You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
 
 Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
