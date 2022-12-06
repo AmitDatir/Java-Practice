87946e99936152de83920c6846286db1af7654ab
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-12-06 22:39:10 +0530

    Update CharVariableToInteger.java

--- General/CharVariableToInteger.java
+++ General/CharVariableToInteger.java
@@ -1,1 +1,20 @@
+public class ChartToInt {
+  public static void main(String[] args) {
+    char a = '5';
+    char b = 'c';
 
+    int num1 = a;                 // ASCII value of characters is assigned
+    int num2 = b;
+
+    System.out.println(num1);    
+    System.out.println(num2);    
+  }
+}
+
+
+
+/*
+Output:
+53
+99
+*/
