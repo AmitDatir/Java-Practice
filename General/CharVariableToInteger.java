34b0f72c0da4e3d1f044ecb595972bc2d2495f57
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2022-12-08 23:47:15 +0530

    Update CharVariableToInteger.java

--- General/CharVariableToInteger.java
+++ General/CharVariableToInteger.java
@@ -19,3 +19,62 @@ Output:
 99
 */
 
+
+//using getNumericValue() method:
+public class ChartToInt {
+  public static void main(String[] args) {
+    char a = '5';
+    char b = 'c';
+
+    int num1 = Character.getNumericValue(a);         //returns numeric value
+    int num2 = Character.getNumericValue(b);
+
+    System.out.println(num1);    
+    System.out.println(num2);    
+  }
+}
+
+
+/*
+Output:
+5
+12
+*/
+
+
+//using parseInt() method:
+public class ChartToInt {
+  public static void main(String[] args) {
+    char a = '5';
+    char b = '7';
+
+    int num1 = Integer.parseInt(String.valueOf(a));         //String.valueOf() : char variable into string
+    int num2 = Integer.parseInt(String.valueOf(b));         //Integer.parseInt(): string into integer
+
+    System.out.println(num1);    
+    System.out.println(num2);    
+  }
+}
+
+
+/*
+Output:
+5
+7
+*/
+
+
+
+//by substracting '0':
+public class ChartToInt {
+  public static void main(String[] args) {
+    char a = '5';
+    char b = '7';
+
+    int num1 = a - '0';
+    int num2 = b - '0';
+
+    System.out.println(num1);    
+    System.out.println(num2);    
+  }
+}
