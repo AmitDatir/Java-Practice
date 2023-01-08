b7baee9fd4b1398df71fa0fb51b359f325e0c314
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-01-08 19:20:34 +0530

    Update BooleanToString.java

--- General/Primitive type conversion/BooleanToString.java
+++ General/Primitive type conversion/BooleanToString.java
@@ -4,10 +4,10 @@ public class BooleanToString1 {
         boolean b1 = true;
         boolean b2 = false;
         
-        String S1 = String.valueOf(b1);
-        String S2 = String.valueOf(b2);
-        System.out.println("String1: " + S1);
-        System.out.println("String2: " + S2);
+        String s1 = String.valueOf(b1);
+        String s2 = String.valueOf(b2);
+        System.out.println("String1: " + s1);
+        System.out.println("String2: " + s2);
     }
 }
 
@@ -27,9 +27,9 @@ public class BooleanToString2 {
         boolean b1 = true;
         boolean b2 = false;
         
-        String S1 = Boolean.toString(b1);
-        String S2 = Boolean.toString(b2);
-        System.out.println("String1: " + S1);
-        System.out.println("String2: " + S2);
+        String s1 = Boolean.toString(b1);
+        String s2 = Boolean.toString(b2);
+        System.out.println("String1: " + s1);
+        System.out.println("String2: " + s2);
     }
 }
