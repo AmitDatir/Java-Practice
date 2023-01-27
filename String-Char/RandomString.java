e3a1370f181580f22b0f2df56b54aabf43c1c9b8
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-01-27 23:33:28 +0530

    Update RandomString.java

--- String-Char/RandomString.java
+++ String-Char/RandomString.java
@@ -25,3 +25,37 @@ Output:
 2. Random string generated: QLSHZDN
 3. Random string generated: GQOKLEP
 */
+
+
+
+//Generate random AlphaNumeric String:
+import java.util.Random;
+public class RandomAlphaNum {
+    public static void main(String args[]) {
+        String uAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lAlphabets = "abcdefghijklmnopqrstuvwxyz";
+        String num = "0123456789";
+        String alphaNumeric = uAlphabets + lAlphabets + num;
+        
+        StringBuilder sb = new StringBuilder();          //random string builder
+        Random random = new Random();                    //object of Random class
+        int length = 11;                                  //to generate random string of 7 char
+        
+        for(int i = 0; i < length; i++){
+            int index = random.nextInt(alphaNumeric.length());        //generating random index from String alphabets
+            char randomChar = alphaNumeric.charAt(index);
+            sb.append(randomChar);
+        }
+        
+        String randomAlphaNumeric = sb.toString();
+        System.out.println("Random string generated: " + randomAlphaNumeric);
+    }
+}
+
+
+/*
+Output:
+1. Random string generated: WRsH5CxDNf6
+2. Random string generated: 5emSop8ReBG
+3. Random string generated: JLmdrbGvFct
+*/
