8c93e1e4c170c9fe81c664d2c88be91e1f5156b8
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-02-15 14:34:47 +0530

    Update FindKeyByValue.java

--- Java Map/FindKeyByValue.java
+++ Java Map/FindKeyByValue.java
@@ -13,17 +13,29 @@ public class FindKey {
         capitals.put("Shri-Lanka", "Colombo");
         System.out.println("Initial Hashmap: " + capitals);
         
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the string value to find key: ");
-        String value = input.next();
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter the string value to find key: ");
+        // String value = new String();
+        // value = input.nextLine();
+        String value = "Washington";
+        System.out.println("Given value is: " + value);
         
         for(Entry<String, String> entry : capitals.entrySet()){
             if(entry.getValue() == value){
                 System.out.println("Key for the given value: " + entry.getKey());
                 break;
-            } else {
-                System.out.println("Please enter correct string.");
             }
+            // else {
+            //     System.out.println("Please enter correct string.");
+            // }
         }
     }
 }
+
+
+/*
+Output:
+Initial Hashmap: {United States=Washington, Japan=Tokyo, England=London, Shri-Lanka=Colombo, Nepal=Kathmandu, India=New Delhi}
+Given value is: Washington
+Key for the given value: United States
+*/
