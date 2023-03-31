63096386093173a6ca9cf3bcbbd4f0792407fe6d
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-03-31 14:13:51 +0530

    Update PalindromicArray.java

--- Arrays/PalindromicArray.java
+++ Arrays/PalindromicArray.java
@@ -7,28 +7,56 @@ public class PalindromicMatrix {
         int len = input.nextInt();
         int arr[] = new int[len];
     
-        System.out.print("Enter elements inside array: ");
+        System.out.println("Enter elements inside array: ");
         for(int i=0; i<len; i++){
             arr[i] = input.nextInt();
         }
-        System.out.println(isPalindrome(arr));
+        String str = isPalindrome(arr);
+        System.out.println(str);
     }
     
     public static String isPalindrome(int arr[]){
         for(int i=0; i<arr.length; i++){
             int reversed = 0;
+            int remainder = 0;
             int num = arr[i];
-            while (arr[i] != 0){
-                int remainder = num % 10;
-                reversed = reversed*10 + remainder;
-                num/=10;
+            while (num != 0){
+                remainder = num % 10;
+                reversed = reversed * 10 + remainder;
+                num /= 10;
             }
-            if (num != reversed){
-                String str1 = "Given array is not Palindromic";
+            if (arr[i] != reversed){
+                String str1 = "Given array is not Palindromic.";
                 return str1;
             }
         }
-        String str2 = "Given array is Palindromic";
+        String str2 = "Given array is Palindromic.";
         return str2;
     }
 }
+
+
+/*
+Output:
+1.
+Enter length of matrix: 
+4
+Enter elements inside array: 
+151
+959
+789
+363
+Given array is not Palindromic.
+
+2.
+Enter length of matrix: 
+6
+Enter elements inside array: 
+999
+787
+494
+111
+323
+686
+Given array is Palindromic.
+*/
