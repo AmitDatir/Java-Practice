cc8e3f0a01e36250603c4ac387688765260b8a3a
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-04-08 00:00:27 +0530

    Update PrimeNumber.java

--- Numbers/PrimeNumber.java
+++ Numbers/PrimeNumber.java
@@ -65,3 +65,33 @@ public class PrimeNumber2 {
         }
     }
 }
+
+
+
+
+//Optimized:
+import java.util.Scanner;
+
+public class PrimeNumber3 {
+    public static void main(String args[]) {
+        int num;
+        boolean result = false;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's a prime number or not: ");
+        num = input.nextInt();
+        
+        for (int i=2; i * i <= num; i++){
+            if(num % i == 0){
+                result= true;
+                break;
+            }
+        }
+        
+        if (!result){
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
