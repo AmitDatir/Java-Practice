57970a41db48914c3c2c8b83864d27cefd4ab11d
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-05-29 21:29:23 +0530

    Update FibonacciSeries.java

--- Numbers/FibonacciSeries.java
+++ Numbers/FibonacciSeries.java
@@ -29,6 +29,7 @@ Fibonnaci series till 15 terms is: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
 
 //Using While loop:
 import java.util.Scanner;
+
 public class FibonacciSeries {
 
     public static void main(String args[]) {
