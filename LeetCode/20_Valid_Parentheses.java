a3ee0f7f2eab434cfb6c25cfb1e3ebb437edf96b
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-07-11 22:36:44 +0530

    Create 20_Valid_Parentheses.java

---
+++ LeetCode/20_Valid_Parentheses.java
@@ -0,0 +1,37 @@
+/*
+20. Valid Parentheses: https://leetcode.com/problems/valid-parentheses/description/
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+*/
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
