1e3d0aaae1850415912f876414cda62b3a7b116c
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-07-23 19:08:00 +0530

    Create 118_Pascal's_Triangle.java

---
+++ LeetCode/118_Pascal's_Triangle.java
@@ -0,0 +1,29 @@
+/*
+118. Pascal's Triangle : https://leetcode.com/problems/pascals-triangle/description/
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+*/
+
+
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        if (numRows <=0){
+            return triangle;
+        }
+        for (int i=0; i<numRows; i++){
+            List<Integer> row =  new ArrayList<Integer>();
+            for (int j=0; j<i+1; j++){
+                if (j==0 || j==i){
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
