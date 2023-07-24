8780f0cf179f323e41c8bf66904902639b2205ee
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-07-24 19:45:54 +0530

    Create 119_Pascal's_Triangle_II.java

---
+++ LeetCode/119_Pascal's_Triangle_II.java
@@ -0,0 +1,38 @@
+/*
+119. Pascal's Triangle II : https://leetcode.com/problems/pascals-triangle-ii/description/
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+*/
+
+
+
+//1st:
+public class Solution {
+    public List<Integer> getRow(int k) {
+        Integer[] arr = new Integer[k + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        
+        for (int i = 1; i <= k; i++) 
+            for (int j = i; j > 0; j--) 
+                arr[j] = arr[j] + arr[j - 1];
+        
+        return Arrays.asList(arr);
+    }
+}
+
+
+//2nd:
+class Solution {
+    public List<Integer> getRow(int r) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(1);
+        long temp = 1;
+        for(int i=1,up=r,down=1;i<=r;i++,up--, down++){
+            temp=temp*up/down;
+            ans.add((int)temp);
+        }
+        return ans;
+    }
+}
