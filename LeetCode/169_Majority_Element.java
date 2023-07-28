acf75b65f453d7836bda419daefd8240084f204a
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-07-28 22:34:10 +0530

    Create 169_Majority_Element.java

---
+++ LeetCode/169_Majority_Element.java
@@ -0,0 +1,56 @@
+/*
+169. Majority Element : https://leetcode.com/problems/majority-element/description/
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+*/
+
+
+//Sorting:
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+    }
+}
+
+
+//Hashmap:
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        n = n / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
+
+
+
+//Moore Voting Algorithm:
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
