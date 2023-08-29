350bbf63a4e22342c9a1cf5ad44202d1c24c7c06
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-08-29 22:58:04 +0530

    Update AverageUsingArray.java

--- Arrays/AverageUsingArray.java
+++ Arrays/AverageUsingArray.java
@@ -5,14 +5,12 @@ public class Average {
     public static void main(String args[]) {
         double average, sum = 0.0;
         double arr[] = new double[]{50.6, 24.3, 69, 96.5, 52.4, 78.6, 12.9};
-        
         System.out.println("Given array elements are: 50.6, 24.3, 69, 96.5, 52.4, 78.6, 12.9.");
         
         for (double num: arr){
             sum += num;
         }
-        average = sum / arr.length;
-        
+        average = sum / arr.length; 
         System.out.println("The average of given elements: " + average);
     }
 }
