bf4af821e8ca26c54c96e294cc8d02bedee7ed81
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-09-19 20:18:07 +0530

    Create 392_Is_Subsequence.java

---
+++ LeetCode/392_Is_Subsequence.java
@@ -0,0 +1,23 @@
+/*
+392. Is Subsequence : https://leetcode.com/problems/is-subsequence/description/
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+
+
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int indexS = 0, indexT = 0;
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
+            }
+            indexT++;
+        }
+        return false;
+    }
+}
