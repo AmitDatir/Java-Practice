2288ed946a8931f8606946158f31048901825349
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-11-10 17:54:33 +0530

    Update Pyramid&Patterns.java

--- General/Pattern/Pyramid&Patterns.java
+++ General/Pattern/Pyramid&Patterns.java
@@ -211,3 +211,40 @@ Output:
 1 2 
 1
 */
+
+
+
+
+
+//0-1 pyramid:
+public class NewTest {
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i; j++) {
+				if(!(j%2==0)) {
+				System.out.print("1");
+				} else {
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
+
+/*
+Output:
+1
+10
+101
+1010
+10101
+101010
+1010101
+10101010
+101010101
+1010101010
+*/
