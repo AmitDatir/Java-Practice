5e52b04ebcae93d8b3f9b8f3688defdbe8e49e24
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-11-16 20:14:07 +0530

    Update FibonacciSeries.java

--- Numbers/FibonacciSeries.java
+++ Numbers/FibonacciSeries.java
@@ -113,3 +113,26 @@ Output:
 Enter the nth number of fibonacci series to find: 8
 8th number of Fibonnaci series is: 21
 */
+
+
+
+
+
+//Using recursion:-
+import java.io.*;
+
+class fibonacci {
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String args[])
+    {
+        int n = 9;
+        System.out.println(
+            n + "th Fibonacci Number: " + fib(n));
+    }
+}
