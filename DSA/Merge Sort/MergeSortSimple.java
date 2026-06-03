f1a5dee8af0f43f14b8684c4e7918f0fdb1cc757
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-03 18:23:32 +0530

    Update MergeSortSimple.java

--- DSA/Merge Sort/MergeSortSimple.java
+++ DSA/Merge Sort/MergeSortSimple.java
@@ -1,1 +1,40 @@
+import java.util.Arrays;
 
+public class Main {
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        System.arraycopy(arr, 0, left, 0, left.length);
+        System.arraycopy(arr, mid, right, 0, right.length);
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 5, 1, 8, 3};
+        mergeSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
