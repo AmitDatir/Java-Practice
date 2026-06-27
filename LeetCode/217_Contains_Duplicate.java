e722270aff1374a955617515daf2e4f9d87170bd
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-27 06:23:46 +0530

    Implement containsDuplicate using HashSet and sorting
    Added a new implementation of containsDuplicate using HashSet and sorting method.

--- LeetCode/217_Contains_Duplicate.java
+++ LeetCode/217_Contains_Duplicate.java
@@ -21,6 +21,25 @@ class Solution {
 }
 
 
+//2:
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            // If the number is already in the set, we found a duplicate
+            if (seen.contains(num)) {
+                return true;
+            }
+            // Otherwise, add the number to the set
+            seen.add(num);
+        }
+        // No duplicates found after scanning the entire array
+        return false;
+    }
+}
+
+
+
 //Simple loops:
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -31,6 +50,27 @@ class Solution {
                 }
             }
         }
+        return false;
+    }
+}
+
+
+
+//2:
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // Sort the array in ascending order
+        Arrays.sort(nums);
+        
+        // Check adjacent elements
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        
         return false;
     }
 }
