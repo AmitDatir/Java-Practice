6f93f83e56d32986c072c168f35cb9573e6d71db
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-27 07:11:49 +0530

    Add twoSum method using hashmap for optimization
    Implemented twoSum method using a hashmap for efficient lookup.

--- LeetCode/1_Two_Sum.java
+++ LeetCode/1_Two_Sum.java
@@ -20,3 +20,23 @@ class Solution {
         return new int[] {};
     }
 }
+
+
+//using hashmap:
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Map to store the number as the key and its index as the value
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            // Check if the complement already exists in the map
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            // Store the current number and its index in the map
+            numMap.put(nums[i], i);
+        }
+        // Return an empty array if no solution is found
+        return new int[] {};
+    }
+}
