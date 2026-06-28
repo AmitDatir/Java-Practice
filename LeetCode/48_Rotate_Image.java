9f0e386ccea4462ed92428e182a1b3575995d310
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-28 15:38:17 +0530

    Add method to rotate image in-place
    Implement in-place rotation of an n x n matrix by 90 degrees clockwise.

---
+++ LeetCode/48_Rotate_Image.java
@@ -0,0 +1,36 @@
+/*
+48. Rotate Image: https://leetcode.com/problems/rotate-image/submissions/
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // Step 1: Transpose the matrix (swap matrix[i][j] with matrix[j][i])
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Step 2: Reverse each row horizontally
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
