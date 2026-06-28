8bb291bf1d20b2602aab1574bc780b4297453264
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-28 16:26:46 +0530

    Implement climbStairs method for climbing stairs problem

--- LeetCode/70_Climbing_Stairs.java
+++ LeetCode/70_Climbing_Stairs.java
@@ -33,3 +33,24 @@ class Solution {
         return curr;
     }
 }
+
+
+
+
+//3:
+class Solution {
+    public int climbStairs(int n) {
+        // Base cases
+        if (n <= 2) {
+            return n;
+        }
+        int prev2 = 1; // Ways to reach step 1
+        int prev1 = 2; // Ways to reach step 2
+        int current = 0;
+        for (int i = 3; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }        return prev1;
+    }
+}
