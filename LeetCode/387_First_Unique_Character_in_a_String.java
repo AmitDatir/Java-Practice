5292d1d2664f870d69e3b7273773c522a9c508d1
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-28 12:28:52 +0530

    Implement first unique character search with hashmap
    Added an alternative implementation using a hashmap to find the first unique character in a string.

--- LeetCode/387_First_Unique_Character_in_a_String.java
+++ LeetCode/387_First_Unique_Character_in_a_String.java
@@ -4,7 +4,7 @@ Given a string s, find the first non-repeating character in it and return its in
 */
 
 
-
+//frequency array:
 public class Solution {
     public int firstUniqChar(String s) {
         int freq [] = new int[26];
@@ -14,5 +14,26 @@ public class Solution {
             if(freq [s.charAt(i) - 'a'] == 1)
                 return i;
         return -1;
+    }
+}
+
+
+
+
+//hashmap:
+class Solution {
+    public int firstUniqChar(String s) {
+        // Step 1: count how many times each character appears
+       Map<Character,Integer> map=new HashMap<>();
+       for(char ch : s.toCharArray()){
+             map.put(ch,map.getOrDefault(ch,0)+1);
+           }
+           // Step 2: scan in order, return index of first char with count == 1
+           for(int i=0;i<s.length();i++){
+             if(map.get(s.charAt(i))==1){   // appears exactly once -> first unique
+                   return i;
+                 }
+           }
+       return -1;   // no unique character found
     }
 }
