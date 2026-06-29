feb3559cd18d8b1e4228e3fb9c2830c6b0ec78f0
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2026-06-29 10:40:48 +0530

    Add invertTree method for binary tree inversion
    Implemented the invertTree method for inverting a binary tree using different approaches.

---
+++ LeetCode/226._Invert_Binary_Tree.java
@@ -0,0 +1,103 @@
+/*
+226. Invert Binary Tree: https://leetcode.com/problems/invert-binary-tree/
+Given the root of a binary tree, invert the tree, and return its root.
+*/
+
+
+
+//1:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // Base case: empty node
+        if (root == null) {
+            return null;
+        }
+        
+        // Post-order / Pre-order recursive swap
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        
+        return root;
+    }
+}
+
+
+
+
+//2:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
+
+
+
+//3:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)return null;
+        TreeNode temp=root.left;
+        root.left=root.right;
+
+        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
